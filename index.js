af5d7b0e52769a98fe7353a7bd510e89553837c9
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-02 21:20:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function happyHolidaysTo(name) {
 return 'Happy holidays, you!'
 }
 function holidayCountdown(holiday, days) {
-  return " It's 20 days until Mother's Day!"
+  return "It's ${days} days until ${holiday}!"
 }
