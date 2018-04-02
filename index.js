4cf59c0fbb1aa5948c11e0967cefb278cdfec6d6
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-04-02 21:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays() {
 return "Happy holidays!" 
 }
 function happyHolidaysTo(holiday, name) {
-return " Happy ${holiday}, ${name}!"
+return 'Happy ${holiday}, ${name}!'
 }
 function happyHolidaysTo(name) {
 return 'Happy holidays, you!'
